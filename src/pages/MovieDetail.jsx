bef44fc8c14f3fea7b38563c9fcacfad67dda945
123 <wlsrhaqhddk@naver.com>
2023-09-14 12:10:43 +0900

    오버뷰 css 수정

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { IMG_BASE_URL } from '../components/Movie';
+import { flushSync } from 'react-dom';
 
 export default function MovieDetail() {
   const { title } = useParams();
@@ -8,16 +9,29 @@ export default function MovieDetail() {
 
   return (
     <div className='page-container'>
-      <div style={{ display: 'flex' }}>
+      <div
+        style={{
+          display: 'flex',
+        }}
+      >
         <img
           style={{ width: '300px', height: '450px' }}
           src={IMG_BASE_URL + state.poster_path}
           alt='영화 포스터 이미지'
         />
-        <div>
-          <div style={{ fontSize: '32px' }}>{title}</div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            margin: '18px',
+            width: ' 550px',
+          }}
+        >
+          <div>
+            <div style={{ fontSize: '42px', margin: '8px' }}>{title}</div>
+          </div>
+          <p>{state.overview}</p>
         </div>
-        <p>{state.overview}</p>
       </div>
     </div>
   );
